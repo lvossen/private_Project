d10d1cd4529c394ce5a10344aed20408bda7309d
LouisVssn <lvossen@ukaachen.de>
2020-06-10 16:57:36 +0200

    pushed

---
+++ src/main/java/com/example/privateProject/PrivateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.privateProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/privateProject/PrivateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.privateProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
